dbd256bab77a6d67489c2b23126340dffdd9221c
MSI <wydrasz3@gmail.com>
2020-02-09 11:15:18 +0100

    walidator

---
+++ src/main/java/pl/sda/person/PersonRepository.java
@@ -0,0 +1,7 @@
+package pl.sda.person;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonRepository {
+}
--- src/main/java/pl/sda/person/PersonValidator.java
+++ src/main/java/pl/sda/person/PersonValidator.java
@@ -6,6 +6,23 @@ import org.springframework.stereotype.Component;
 public class PersonValidator extends AbstractPersonValidator {
 
     public boolean validate(Person person) {
-        return false;
+        boolean isFirstNameValid = validateName(person.getFirstName(), 3, 100);
+        boolean isLastNameValid = validateName(person.getLastName(), 2, 200);
+        boolean isAgeValid = validateAge(person.getAge());
+        return isFirstNameValid && isLastNameValid && isAgeValid;
     }
+
+
+    private boolean validateName(String name, int minLength, int maxLength) {
+        return name != null && name.length() > minLength && name.length() < maxLength && isContainsVowelAndNotSpecial(name);
+    }
+
+    private boolean isContainsVowelAndNotSpecial(String name) {
+        return name.matches(".*[AEIOUaeiou].*") && !name.matches("[^a-zA-Z0-9 ]");
+    }
+
+    private boolean validateAge(int age) {
+        return age > 0 && age < 150;
+    }
+
 }
